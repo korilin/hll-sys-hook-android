5adba7e7ef819ec6cb25fd07ad8b310a229ef257
korilin <korilin.dev@gmail.com>
2025-09-22 19:13:41 +0800

    dist: redistribute 1.4-KORILIN-V15FIX-3, try support 16KB

--- lib_thread_shield/build.gradle.kts
+++ lib_thread_shield/build.gradle.kts
@@ -89,7 +89,7 @@ mavenPublishing {
     publishToMavenCentral(true)
     signAllPublications()
 
-    coordinates("io.github.korilin", "hll-sys-hook-android","1.4-KORILIN-V15FIX-2")
+    coordinates("io.github.korilin", "hll-sys-hook-android","1.4-KORILIN-V15FIX-3")
 
     pom {
         name = "HuoLaLa unify hook lib"
